95ac621411dfdae74f3875865a0d05f427133985
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-04-25 16:12:22 -0300

    Fields for coverage statistics

--- coveragestore.py
+++ coveragestore.py
@@ -5,11 +5,23 @@ from cassandra.cqlengine.models import Model
 class SampleCoverage(Model):
     sample = columns.Text(primary_key=True, partition_key=True)
 
-    library_name = columns.Text(primary_key=True)
     run_id = columns.Text(primary_key=True)
+    library_name = columns.Text(primary_key=True)
+    program_name = columns.Text(primary_key=True)
+
+    amplicon_depth = columns.Map(columns.Text, columns.Integer)
+    thresholds = columns.List(columns.Integer)
+    perc_bp_cov_at_thresholds = columns.Map(columns.Integer, columns.Float)
 
 
 class AmpliconCoverage(Model):
     amplicon = columns.Text(primary_key=True, partition_key=True)
 
+    sample = columns.Text(primary_key=True)
+    run_id = columns.Text(primary_key=True)
+    library_name = columns.Text(primary_key=True)
+    program_name = columns.Text(primary_key=True)
 
+    depth = columns.Integer()
+    thresholds = columns.List(columns.Integer)
+    perc_bp_cov_at_thresholds = columns.Map(columns.Integer, columns.Float)
