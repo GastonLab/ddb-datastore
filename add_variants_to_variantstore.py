3f590e4dfa6bbf047de35aa49c6103d069e6d8c1
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-04-25 15:13:19 -0300

    bug fix

--- add_variants_to_variantstore.py
+++ add_variants_to_variantstore.py
@@ -225,10 +225,10 @@ def get_population_freqs(variant):
 
 
 def get_amplicon_data(variant):
-    data = {'amplicon': variant.INFO.get('amplicon') or None,
-            'amplicon_myeloid': variant.INFO.get('amplicon_myeloid') or None,
-            'ampliconA': variant.INFO.get('ampliconA') or None,
-            'ampliconB': variant.INFO.get('ampliconB') or None}
+    data = {'amplicon': variant.INFO.get('amplicon') or "None",
+            'amplicon_myeloid': variant.INFO.get('amplicon_myeloid') or "None",
+            'ampliconA': variant.INFO.get('ampliconA') or "None",
+            'ampliconB': variant.INFO.get('ampliconB') or "None"}
     if data['amplicon'] or data['amplicon_myeloid'] or data['ampliconA'] or data['ampliconB']:
         data['in_amplicon'] = "True"
     else:
