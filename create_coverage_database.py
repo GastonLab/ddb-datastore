ba47aa4da6315a6a90b081c9e96f015539dfe760
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-04-26 09:50:08 -0300

    tweak

--- create_coverage_database.py
+++ create_coverage_database.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         password = getpass.getpass()
         auth_provider = PlainTextAuthProvider(username=args.username, password=password)
         cluster = Cluster([args.address], auth_provider=auth_provider)
-        session = cluster.connect(row_factory=query.dict_factory())
+        session = cluster.connect(row_factory=query.dict_factory)
         # connection.setup([args.address], None, auth_provider=auth_provider)
     else:
         cluster = Cluster([args.address])
