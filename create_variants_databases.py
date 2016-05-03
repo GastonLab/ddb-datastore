62679a59569dbd59d9e460a5e16fd27f437a866d
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-05-03 09:41:02 -0300

    Bug fix

--- create_variants_databases.py
+++ create_variants_databases.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--address', help="IP Address for Cassandra connection", default='127.0.0.1')
     parser.add_argument('-u', '--username', help='Cassandra username for login', default=None)
+    parser.add_argument('-r', '--replication_factor', help="Cassandra replication factor", default=3)
     args = parser.parse_args()
 
     if args.username:
@@ -22,7 +23,7 @@ if __name__ == "__main__":
         auth_provider = PlainTextAuthProvider(username=args.username, password=password)
         cluster = Cluster([args.address], auth_provider=auth_provider)
         session = cluster.connect()
-        session.row_factory=query.dict_factory
+        session.row_factory = query.dict_factory
     else:
         cluster = Cluster([args.address])
         session = cluster.connect()
