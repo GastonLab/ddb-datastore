6f7db46a65acefa866a94933441cd47e4d4c160d
dgaston <daniel.gaston@deaddriftbio.com>
2016-08-25 14:26:55 -0300

    Starting script for better automated gathering of validation variants

---
+++ validate_variants.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import argparse
+import getpass
+import sys
+
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.cqlengine import connection
+from ddb import configuration
+
+import utils
+from variantstore import Variant
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--samples_file', help="Input configuration file for samples")
+    parser.add_argument('-c', '--configuration', help="Configuration file for various settings")
+    parser.add_argument('-r', '--report', help="Root name for reports (per sample)")
+    parser.add_argument('-a', '--address', help="IP Address for Cassandra connection", default='127.0.0.1')
+    parser.add_argument('-u', '--username', help='Cassandra username for login', default=None)
+    parser.add_argument('-v', '--variant_callers', help="Comma-delimited list of variant callers used")
+
+    args = parser.parse_args()
+
+    sys.stdout.write("Parsing configuration data\n")
+    config = configuration.configure_runtime(args.configuration)
+
+    sys.stdout.write("Parsing sample data\n")
+    samples = configuration.configure_samples(args.samples_file, config)
+
+    if args.username:
+        password = getpass.getpass()
+        auth_provider = PlainTextAuthProvider(username=args.username, password=password)
+        connection.setup([args.address], "variantstore", auth_provider=auth_provider)
+    else:
+        connection.setup([args.address], "variantstore")
+
+    callers = ['mutect', 'freebayes', 'scalpel', 'vardict', 'platypus', 'pindel']
