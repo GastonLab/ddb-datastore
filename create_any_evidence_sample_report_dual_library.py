2d0b81ecd1ab5a25b182274e2b940c93b0db3ae8
Dan Gaston <daniel.gaston@gmail.com>
2017-02-14 09:35:23 -0400

    updates

--- create_any_evidence_sample_report_dual_library.py
+++ create_any_evidence_sample_report_dual_library.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--samples_file', help="Input configuration file for samples")
     parser.add_argument('-c', '--configuration', help="Configuration file for various settings")
-    parser.add_argument('-r', '--report', help="Root name for reports (per sample)")
+    parser.add_argument('-r', '--report', help="Root name for reports (per sample)", default='any_evidence_report')
     parser.add_argument('-a', '--address', help="IP Address for Cassandra connection", default='127.0.0.1')
     parser.add_argument('-u', '--username', help='Cassandra username for login', default=None)
 
