ad5eb634f64ebb2271cb9ca0122b8a9d41ab4b36
Dan Gaston <daniel.gaston@gmail.com>
2017-05-16 12:02:09 -0300

    Fixing ordering

--- report_xlwt.py
+++ report_xlwt.py
@@ -92,7 +92,8 @@ def process_sample(job, config, sample, samples, addresses, authenticator, thres
                     ).allow_filtering()
 
                     num_matches = match_variants.count()
-                    ordered_var = match_variants.order_by('sample', 'library_name', 'run_id').limit(num_matches + 1000)
+                    ordered_var = match_variants.order_by('pos', 'ref', 'alt', 'sample', 'library_name',
+                                                          'run_id').limit(num_matches + 1000)
                     vafs = list()
                     for var in ordered_var:
                         vaf = var.max_som_aaf
