b254ac0a78d930d467c7cc56a2936add0197b867
Dan Gaston <daniel.gaston@gmail.com>
2017-08-04 13:43:12 -0300

    Adding mean coverage calculation to output

--- merkel_count_bases_by_coverage.py
+++ merkel_count_bases_by_coverage.py
@@ -25,16 +25,20 @@ if __name__ == "__main__":
     samples = configuration.configure_samples(args.samples_file, config)
 
     with open("merkel_sequenced_bases_passing_depth.txt", 'w') as output:
-        output.write("Sample\tKB passing\tTotal KB\tFraction")
+        output.write("Sample\tMean Coverage\tKB passing\tTotal KB\tFraction")
         for sample in samples:
             num_bases = 0.0
             total_bases = 0.0
+            total_coverage = 0.0
             with open("{}.recalibrated.sorted.bam.bed".format(sample), 'r') as coverage_file:
                 sys.stdout.write("Reading base-by-base coverage data for {}\n".format(sample))
                 reader = csv.reader(coverage_file, dialect='excel-tab')
                 reader.next()
                 for row in reader:
                     total_bases += 1
+                    total_coverage += int(row[2])
                     if int(row[2]) >= int(args.min_depth):
                         num_bases += 1.0
-                output.write("{}\t{}\t{}\t{}\n".format(sample, num_bases / 1000, total_bases / 1000, num_bases / total_bases))
+                mean_coverage = total_coverage / total_bases
+                output.write("{}\t{}\t{}\t{}\n".format(sample, mean_coverage, num_bases / 1000, total_bases / 1000,
+                                                       num_bases / total_bases))
