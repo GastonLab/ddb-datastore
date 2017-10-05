d746e061ffb2f4d164b9375c10f2c02a5ba79b93
Dan Gaston <daniel.gaston@gmail.com>
2017-10-05 10:16:52 -0300

    Updating coverage analysis script

--- coverage_report.py
+++ coverage_report.py
@@ -11,9 +11,7 @@ from scipy import stats
 from toil.job import Job
 from ddb import configuration
 from ddb_ngsflow import pipeline
-from variantstore import Variant
 from collections import defaultdict
-from coveragestore import SampleCoverage
 from coveragestore import AmpliconCoverage
 from cassandra.cqlengine import connection
 from cassandra.auth import PlainTextAuthProvider
@@ -29,22 +27,59 @@ if __name__ == "__main__":
     args = parser.parse_args()
     args.logLevel = "INFO"
 
+    if args.username:
+        password = getpass.getpass()
+        auth_provider = PlainTextAuthProvider(username=args.username, password=password)
+        connection.setup([args.address], "coveragestore", auth_provider=auth_provider)
+    else:
+        connection.setup([args.address], "coveragestore")
+
     amplicons_list = list()
+    amplicon_coverage_stats = defaultdict(dict)
+    amplicon_stats_by_month = defaultdict(lambda: defaultdict(dict))
+
     target_amplicons = utils.get_target_amplicons(args.list)
     for amplicon in target_amplicons:
         if amplicon not in amplicons_list:
             amplicons_list.append(amplicon)
 
     for amplicon in target_amplicons:
-        coverage_data = SampleCoverage.objects.timeout(None).filter(
-            SampleCoverage.sample == samples[sample][library]['sample_name'],
-            SampleCoverage.amplicon == amplicon,
-            SampleCoverage.run_id == samples[sample][library]['run_id'],
-            SampleCoverage.library_name == samples[sample][library]['library_name'],
-            SampleCoverage.program_name == "sambamba"
+        coverage_values = list()
+        coverage_values_by_month = defaultdict(list)
+        coverage_data = AmpliconCoverage.objects.timeout(None).filter(
+            AmpliconCoverage.amplicon == amplicon
         )
-        ordered_amplicons = coverage_data.order_by('amplicon', 'run_id').limit(coverage_data.count() + 1000)
-        for result in ordered_amplicons:
-            reportable_amplicons.append(result)
-            target_amplicon_coverage[amplicon] = result
-            ordered_amplicon_coverage.append(result)
+        ordered_samples = coverage_data.order_by('sample', 'run_id').limit(coverage_data.count() + 1000)
+        for result in ordered_samples:
+            coverage_values.append(result.mean_coverage)
+            yr_month_id = result.run_id[:4]
+            coverage_values_by_month[yr_month_id].append(result.mean_coverage)
+
+        amplicon_coverage_stats[amplicon]['median'] = np.median(coverage_values)
+        amplicon_coverage_stats[amplicon]['std_dev'] = np.std(coverage_values)
+        amplicon_coverage_stats[amplicon]['min'] = np.amin(coverage_values)
+        amplicon_coverage_stats[amplicon]['max'] = np.amax(coverage_values)
+
+        for yr_month_id in coverage_values_by_month:
+            amplicon_stats_by_month[amplicon][yr_month_id]['median'] = np.median(coverage_values)
+            amplicon_stats_by_month[amplicon][yr_month_id]['std_dev'] = np.std(coverage_values)
+            amplicon_stats_by_month[amplicon][yr_month_id]['min'] = np.amin(coverage_values)
+            amplicon_stats_by_month[amplicon][yr_month_id]['max'] = np.amax(coverage_values)
+
+    with open("coverage_analysis.txt", "w") as coverage_report:
+        coverage_report.write("Amplicon\tMedian\Std\tmin\tmax\n")
+        for amplicon in amplicon_coverage_stats:
+            coverage_report.write("{}\t{}\t{}\t{}\t{}\n".format(amplicon, amplicon_coverage_stats[amplicon]['median'],
+                                                                amplicon_coverage_stats[amplicon]['std_dev'],
+                                                                amplicon_coverage_stats[amplicon]['min'],
+                                                                amplicon_coverage_stats[amplicon]['max']))
+
+    with open("coverage_analysis_by_month.txt", "w") as coverage_report:
+        coverage_report.write("Amplicon\tMonth\tMedian\Std\tmin\tmax\n")
+        for amplicon in amplicon_stats_by_month:
+            for yr_month_id in amplicon_stats_by_month[amplicon]:
+                coverage_report.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(amplicon, yr_month_id,
+                                                                        amplicon_stats_by_month[amplicon][yr_month_id]['median'],
+                                                                        amplicon_stats_by_month[amplicon][yr_month_id]['std_dev'],
+                                                                        amplicon_stats_by_month[amplicon][yr_month_id]['min'],
+                                                                        amplicon_stats_by_month[amplicon][yr_month_id]['max']))
