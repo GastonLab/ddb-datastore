180f98eda2ed6d2cec1a33ca3c326880a87977bf
Dan Gaston <daniel.gaston@gmail.com>
2017-10-05 12:53:41 -0300

    Add by run summary

--- coverage_report.py
+++ coverage_report.py
@@ -31,6 +31,7 @@ if __name__ == "__main__":
     amplicons_list = list()
     amplicon_coverage_stats = defaultdict(dict)
     amplicon_stats_by_month = defaultdict(lambda: defaultdict(dict))
+    amplicon_stats_by_run = defaultdict(lambda: defaultdict(dict))
 
     sys.stdout.write("Parsing Amplicon List\n")
     target_amplicons = utils.get_target_amplicons(args.list)
@@ -43,6 +44,7 @@ if __name__ == "__main__":
         sys.stdout.write("Retrieving Coverage Data for {}\n".format(amplicon))
         coverage_values = list()
         coverage_values_by_month = defaultdict(list)
+        coverage_values_by_run = defaultdict(list)
         coverage_data = AmpliconCoverage.objects.timeout(None).filter(
             AmpliconCoverage.amplicon == amplicon
         )
@@ -51,6 +53,7 @@ if __name__ == "__main__":
             coverage_values.append(result.mean_coverage)
             yr_month_id = result.run_id[:4]
             coverage_values_by_month[yr_month_id].append(result.mean_coverage)
+            coverage_values_by_run[result.run_id].append(result.mean_coverage)
 
         amplicon_coverage_stats[amplicon]['median'] = np.median(coverage_values)
         amplicon_coverage_stats[amplicon]['std_dev'] = np.std(coverage_values)
@@ -63,9 +66,15 @@ if __name__ == "__main__":
             amplicon_stats_by_month[amplicon][yr_month_id]['min'] = np.amin(coverage_values_by_month[yr_month_id])
             amplicon_stats_by_month[amplicon][yr_month_id]['max'] = np.amax(coverage_values_by_month[yr_month_id])
 
+        for run_id in coverage_values_by_run:
+            amplicon_stats_by_run[amplicon][run_id]['median'] = np.median(coverage_values_by_run[run_id])
+            amplicon_stats_by_run[amplicon][run_id]['std_dev'] = np.std(coverage_values_by_run[run_id])
+            amplicon_stats_by_run[amplicon][run_id]['min'] = np.amin(coverage_values_by_run[run_id])
+            amplicon_stats_by_run[amplicon][run_id]['max'] = np.amax(coverage_values_by_run[run_id])
+
     sys.stdout.write("Printing Results\n")
     with open("coverage_analysis_{}.txt".format(args.report), "w") as coverage_report:
-        coverage_report.write("Amplicon\tMedian\Std\tmin\tmax\n")
+        coverage_report.write("Amplicon\tMedian\tStd\tmin\tmax\n")
         for amplicon in amplicon_coverage_stats:
             coverage_report.write("{}\t{}\t{}\t{}\t{}\n".format(amplicon, amplicon_coverage_stats[amplicon]['median'],
                                                                 amplicon_coverage_stats[amplicon]['std_dev'],
@@ -73,7 +82,7 @@ if __name__ == "__main__":
                                                                 amplicon_coverage_stats[amplicon]['max']))
 
     with open("coverage_analysis_by_month-{}.txt".format(args.report), "w") as coverage_report:
-        coverage_report.write("Amplicon\tMonth\tMedian\Std\tmin\tmax\n")
+        coverage_report.write("Amplicon\tMonth\tMedian\tStd\tmin\tmax\n")
         for amplicon in amplicon_stats_by_month:
             for yr_month_id in amplicon_stats_by_month[amplicon]:
                 coverage_report.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(amplicon, yr_month_id,
@@ -81,3 +90,13 @@ if __name__ == "__main__":
                                                                         amplicon_stats_by_month[amplicon][yr_month_id]['std_dev'],
                                                                         amplicon_stats_by_month[amplicon][yr_month_id]['min'],
                                                                         amplicon_stats_by_month[amplicon][yr_month_id]['max']))
+
+    with open("coverage_analysis_by_run-{}.txt".format(args.report), "w") as coverage_report:
+        coverage_report.write("Amplicon\tMonth\tMedian\tStd\tmin\tmax\n")
+        for amplicon in amplicon_stats_by_run:
+            for run_id in amplicon_stats_by_run[amplicon]:
+                coverage_report.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(amplicon, run_id,
+                                                                        amplicon_stats_by_run[amplicon][run_id]['median'],
+                                                                        amplicon_stats_by_run[amplicon][run_id]['std_dev'],
+                                                                        amplicon_stats_by_run[amplicon][run_id]['min'],
+                                                                        amplicon_stats_by_run[amplicon][run_id]['max']))
