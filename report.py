c537bfc03574edbe6982f04f00a05bb0708c6fa6
Daniel Gaston <daniel.gaston@dal.ca>
2019-08-21 16:13:57 -0300

    testing cosmic nums data types and parsing

--- report.py
+++ report.py
@@ -437,7 +437,7 @@ def process_sample(job, config, sample, samples, addresses, authenticator,
 
         row = 1
 
-        sys.stdout.write("{}\n".format(type(variant.cosmic_data['num_samples'])))
+        sys.stderr.write("{}\n".format(type(variant.cosmic_data['num_samples'])))
 
         for variant in report_data['variants'][tier_key[sheet_num]]:
             if "pathogenic" in variant.clinvar_data['significance']:
