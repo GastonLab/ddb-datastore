259e9d80d208a638370bec39fc7737ff3af824f6
Daniel Gaston <daniel.gaston@dal.ca>
2020-04-20 09:48:14 -0300

    Adding filter for only passing variants

--- summarize_tso500_vcfs.py
+++ summarize_tso500_vcfs.py
@@ -112,7 +112,6 @@ if __name__ == "__main__":
                 for v in VCF("{}.vcfanno.vcf.gz".format(row[0])):
                     if len(v.ALT) >= 1:
                         if v.FILTER is None:
-                            print("Variant passing filter found\n")
                             max_aaf = 1.0
                             if v.INFO.get('max_aaf_all'):
                                 max_aaf = v.INFO.get('max_aaf_all')
